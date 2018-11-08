a9acf05ace34f0b555a9c7b2e67737f6aff43f99
Isaac Mcauley <mcauleyisaac@gmail.com>
2018-11-08 15:48:33 -0700

    Added the Y and Z rotation matrices

--- cube.hs
+++ cube.hs
@@ -19,3 +19,5 @@ ma (P x1 y1 z1) (P x2 y2 z2) = P (x1+x2) (y1+y2) (z1+z2)
 
 createRotationMatrix :: Char -> Double -> [P]
 createRotationMatrix 'X' t = [(P 1 0 0), (P 0 (cos t) (-(sin t))), (P 0 (sin t) (cos t))]
+createRotationMatrix 'Y' t = [(P (cos t) 0 (sin t)), (P 0 1 0), (P (-(sin t)) 0 (cos t))]
+createRotationMatrix 'Z' t = [(P (cos t) (-(sin t)) 0), (P (sin t) (cos t) 0), (P 0 0 1)]
